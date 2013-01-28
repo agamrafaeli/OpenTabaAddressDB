f413d7811cf94ea0caa5f6c7ba94112a9999d8e9
Agam Rafaeli <agam.rafaeli@gmail.com>
2013-01-28 16:27:13 +0200

    cleaned up getGushHelka

---
+++ getGushHelka.py
@@ -0,0 +1,30 @@
+#IMPORTS
+import urllib
+import urllib2
+from bs4 import BeautifulSoup
+
+#FUNCTION
+def GetGushHelka(cityid, streetid, houseid):
+    """
+    Gets cityID, streetID, houseID
+    returns a tuple as (GushID,HelkaID)
+
+    Example:
+    gh = GetGushHelka(3000, 1053, 1)
+    print gh[0]                             #will print 30161
+    print "Helka: ", gh[1]         #will print 8
+    """
+    reqURL = 'http://www.gov.il/firstGov/Templates/MapiNewAddressSearch.aspx?NRMODE=Published&NRNODEGUID=%7b426BD068-836D-4C8F-8F82-A3B92215CCD0%7d&NRORIGINALURL=%2fFirstGov%2fTopNav%2fOfficesAndAuthorities%2fOAUList%2fConstSurvey%2ffirstGovGushcalculator%2f&NRCACHEHINT=Guest'
+    urlParams = { 'hiddenInputCityVal': str(cityid),
+               'hiddenInputStreetVal': str(streetid),
+               'hiddenInputHouseNumberVal': str(houseid),
+               '__EVENTTARGET':'lnkBtnSearch',           
+               }
+    
+    data = urllib2.urlopen(reqURL, urllib.urlencode(urlParams)).read()
+    soup = BeautifulSoup(data.decode('utf-8'))
+    row = soup('table', {'id' : 'gush_parcel_TBL'})[0].tbody('tr')[1]
+    gush = int(row('td')[0].contents[0])
+    helka = int(row('td')[1].contents[0])
+    return (gush, helka)
+
