447d428780d943949cb3d85b17df65641dc9d0a4
Agam Rafaeli <agam.rafaeli@gmail.com>
2013-02-04 20:48:29 +0200

    Fixed writeToDB script

--- getStreetNames.py
+++ getStreetNames.py
@@ -1,3 +1,5 @@
+# -*- coding: UTF-8-*-
+
 import re
 import urllib2
 import time
@@ -8,20 +10,23 @@ This file is a script that is supposed to get an array of streets
 """
 
 #CONSTANTS
+GUSH_HELKA_CALCULATOR_URL = "http://www.gov.il/FirstGov/TopNav/OfficesAndAuthorities/OAUList/ConstSurvey/firstGovGushcalculator/"
 GET_STREET_BY_CITY_ID_REQ = "http://www.gov.il/firstgov/services/MapiInformation.aspx/getStreets?cityNum="
 GET_STREET_NUMBERS_REQ = "http://www.gov.il/firstgov/services/MapiInformation.aspx/getNumbers?streetCode="
-JERUSALEM_ID = 3000
 
-def getCityIDS():
+STREET_ID_REGEX = '<street id=\"(\d+)\"><name>(.*?)</name></street>'
+CITY_ID_REGEX = '<option value=\"(\d+)\">(.*?)</option>'
+STREET_NUM_REGEX = '<number>(.*?)</number>'
+
+def getCities():
     """
     Returns an array of tuples of the form (CITY_ID,CITY_NAME) of all the cities
     """
-    gushHelkaResponse = urllib2.urlopen("http://www.gov.il/FirstGov/TopNav/OfficesAndAuthorities/OAUList/ConstSurvey/firstGovGushcalculator/").read()
-    CITY_ID_REGEX = '<option value=\"(\d+)\">(.*?)</option>'
+    gushHelkaResponse = urllib2.urlopen(GUSH_HELKA_CALCULATOR_URL).read()
     cityArr = re.findall(CITY_ID_REGEX,gushHelkaResponse)
     return  cityArr
 
-def getStreetsArrOfCity(cityID):
+def getStreetsByCity(city):
     """
     Gets an ID of a city
     Returns an array of tuples of the form (STREET_ID,STREET_NAME)
@@ -30,44 +35,11 @@ def getStreetsArrOfCity(cityID):
     getStreetsArrOfCity(3000)
     [(1,'Ben-Yehuda'),(2,'King Geroge'),...]
     """
-    jerusalemStreetsResponse = urllib2.urlopen(GET_STREET_BY_CITY_ID_REQ+str(cityID)).read()
-    STREET_ID_REGEX = '<street id=\"(\d+)\"><name>(.*?)</name></street>'
-    streetsArr = re.findall(STREET_ID_REGEX,jerusalemStreetsResponse)
+    streetsResponse = urllib2.urlopen(GET_STREET_BY_CITY_ID_REQ+str(city)).read()
+    streetsArr = re.findall(STREET_ID_REGEX,streetsResponse)
     return streetsArr
 
-def getStreetNumbersResponse(url,tryNum=0,maxTries = 5):
-    """
-    gets the request from the given url
-    if it fails, it waits 0.5 secs and tries again for MAX_TRIES
-    """
-    if tryNum > maxTries:
-        raise LookupError("Connection could not be made to "+str(url))
-    try:
-        retStr = urllib2.urlopen(url).read()
-        return retStr
-    except:
-        time.sleep(0.05)
-        tryNum = tryNum+1
-        return getStreetNumbersResponse(url,tryNum)
-
-def getCityStreetBuildingNumsArr(cityID):
-    """
-    Gets a cityID
-    Returns an array of tuples of the form (streetID,streetName,arrOfBuildings)
-        where arrOfBuildings is an array of the building nums (as strings) that are known
-    """
-    streetsOfCity = getStreetsArrOfCity(cityID)
-    streetsArr = []
-    STREET_NUM_REGEX = '<number>(.*?)</number>'
-    counter= 0
-    for street in streetsOfCity:
-        responseXML = getStreetNumbersResponse(GET_STREET_NUMBERS_REQ+str(street[0]),1)
-        numberStreetArr = re.findall(STREET_NUM_REGEX,responseXML)
-        newStreet = (street[0],street[1],numberStreetArr)
-        streetsArr.append(newStreet)
-        counter = counter +1
-        if counter % 50 == 0:
-            print "Got "+str(counter)+" streets!"
-    return streetsArr
-
-    #streetsArr finishes with an array of tuples of all the streets with their IDs and house numbers
+def getBuildingNumsByStreet(street):
+    responseXML = urllib2.urlopen(GET_STREET_NUMBERS_REQ+str(street)).read()
+    buildingNums = re.findall(STREET_NUM_REGEX,responseXML)
+    return buildingNums
--- writeCitysStreetsBuildings.py
+++ writeCitysStreetsBuildings.py
@@ -3,10 +3,9 @@
 #HARD CODED TO WRITE TO DB THE JERUSALEM STREETS
 #DO NOT USE BEFORE UNDERSTANDING
 
-import sqlite3
+#IMPORTS
+import sqlite3, sys
 import getStreetNames
-import pickle
-import os, sys
 
 if len(sys.argv) != 2:
     print "Usage: %s <dbname>" % sys.argv[0]
@@ -14,39 +13,28 @@ if len(sys.argv) != 2:
 
 db_name = sys.argv[1]
 
-db =sqlite3.connect(db_name)
-curs = db.cursor()
+def writeDB(cursor):
+    cities = getStreetNames.getCities()
+    cityCounter = 0
+    for city in cities:
+        print "STARTING ON CITY: "+ city[1].decode('utf-8')
+        streets = getStreetNames.getStreetsByCity(city[0])
+        streetCounter = 0
+        for street in streets:
+            buildingNums = getStreetNames.getBuildingNumsByStreet(street[0])
+            for num in buildingNums:
+                insertTuple = (city[0],city[1].decode('utf-8'),street[0],street[1].decode('utf-8'),num.decode('utf-8'))
+                cursor.execute(u'INSERT into STREET_GUSH_HELKA (CITY_ID,CITY_NAME,STREET_ID,STREET_NAME,BUILDING_NUM) VALUES (?,?,?,?,?)',insertTuple)
+            streetCounter = streetCounter + 1
+            if streetCounter % 50 == 0:
+                print "DONE "+str(streetCounter)+" STREETS"
+        print "DONE WITH CITY: " + city[1].decode('utf-8')
+        cityCounter = cityCounter+1
+        print "CITIES LEFT: "+str(len(cities)-cityCounter)
 
-def writeBuildingNum(cityID,cityName,streetID,streetName,buildingNum,cursor):
-    insertTuple = (cityID,cityName,streetID,streetName.decode('utf-8'),buildingNum.decode('utf-8'))
-    print "Insert Tuple"
-    cursor.execute(u'INSERT into STREET_GUSH_HELKA (CITY_ID,CITY_NAME,STREET_ID,STREET_NAME,BUILDING_NUM) VALUES (?,?,?,?,?)',insertTuple)
-    print "WRITTEN"
 
+conn = sqlite3.connect(db_name)
 
-
-def writeStreet(cityID,cityName,streetID,streetName,buildingArr,cursor):
-    for building in buildingArr:
-        writeBuildingNum(cityID,cityName,streetID,streetName,building,cursor)
-
-def writeCity(cityID,cityName,cursor):
-    if os.path.exists("pickleDump.pkl"):
-        f=open("pickleDump.pkl","r")
-        streetsArr = pickle.load(f)
-        f.close()
-    else:
-        streetsArr = getStreetNames.getCityStreetBuildingNumsArr(cityID)
-        f=open("pickleDump.pkl","w")
-        pickle.dump(streetsArr,f)
-        f.close()
-    for street in streetsArr:
-        writeStreet(cityID,cityName,street[0],street[1],street[2],cursor)
-
-def writeIsrael(cursor):
-    citysArr = getStreetNames.getCityIDS()
-    for city in citysArr:
-        writeCity(city[0],city[1],cursor)
-
-writeCity(3000,u"ירושלים",curs)
-
-curs.close()
+writeDB(conn)
+conn.commit()
+conn.close()
